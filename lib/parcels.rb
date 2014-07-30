edd697d4bd24e80eca4215f303ab0e3173a5269a
Brandon Madsen & Patrick David <student@epicodus.com>
2014-07-30 15:57:51 -0700

    take the volume multiplied by the weight and return the cost for shipping

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,4 +1,16 @@
 class Parcels
-  def initialize(height, lenght, width, weight)
+  def initialize(height, length, width, weight)
+    @height = height
+    @weight = weight
+    @length = length
+    @width = width
+  end
+
+  def volume
+    @length * @width * @height
+  end
+
+  def shipping
+    @weight * volume
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,8 +2,19 @@ require 'rspec'
 require 'parcels'
 
 describe Parcels do
-  it 'is initialized with dimensions and weight' do
-    test_parcels = Parcels.new(5, 3, 4, 20)
-    test_parcels.should be_an_instance_of Parcels
-  end
-end
+  # describe 'initialize' do
+    it 'is initialized with dimensions and weight' do
+      test_parcels = Parcels.new(5, 3, 4, 20)
+      expect(test_parcels).to be_an_instance_of Parcels
+      end
+
+    it 'calculates the volume of a parcel' do
+    test_parcels = Parcels.new(3, 4, 5, 10)
+    expect(test_parcels.volume).to(eq(60))
+      end
+
+    it 'calculate the cost for shipping' do
+    test_parcels = Parcels.new(3, 2, 2, 5)
+    expect(test_parcels.shipping).to(eq(60))
+      end
+ end
